c9e7273f7cbb05081cf703083fd441c288ab0bc9
Oleg <olegbespalko12@gmail.com>
2019-07-18 14:41:15 +0300

    - Created Redux store - Created Counter on React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Counter from './components/Counter';
+import {createStore, bindActionCreators} from 'redux';
+import * as actions from './actions';
+import reducer from './reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(reducer);
+const {dispatch} = store;
+
+const {dec, rnd, inc} = bindActionCreators(actions, dispatch);
+
+
+const update = () => {
+  ReactDOM.render(
+    <Counter counter={store.getState()}
+             inc={inc}
+             dec={dec}
+             rnd={() => {
+               const value = Math.floor(Math.random() * 10);
+               rnd(value);
+             }}/>,
+    document.getElementById('root'));
+};
+
+update();
+
+store.subscribe(update);
